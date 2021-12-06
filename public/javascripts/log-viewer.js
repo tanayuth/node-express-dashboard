d45a960096a72301568f3fc94b22dc7f40aa552e
yutt <tanayuth@gmail.com>
2021-12-06 22:58:18 +1300

    Display a log file in real time

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,8 +1,13 @@
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector("#log-window");
+const filePath = document.getElementById('logFilePath').value;
+
 connection.onopen = () => {
-    connection.send("Hello from the client!");
+    if(filePath){
+        connection.send(filePath);
+    }
 }
 connection.onmessage = (event) => {
-    logWindow.innerHTML = event.data;
+    const logs = event.data.split('\n').join('<hr>');
+    logWindow.innerHTML = logs
 }
