9adc6de275bd43705ec16e77425eb51ff6e87831
yutt <tanayuth@gmail.com>
2021-12-27 22:16:18 +1300

    Adding settings and configuration

--- services/settings-service.js
+++ services/settings-service.js
@@ -1,19 +1,46 @@
+const fs = require("fs")
+const path = require("path")
+
+const settingsFilePath = path.join(__dirname, "../json/settings.json")
+
+
 function getSettings() {
-  return {}
+    const settingsData = fs.readFileSync(settingsFilePath)
+    return JSON.parse(settingsData)
 }
 
 function writeSettings(newSettings) {
+    const settingsJSON = JSON.stringify(newSettings, null, 2)
+    try {
+        fs.writeFileSync(settingsFilePath, settingsJSON)
+        return true
+    } catch {
+        return false
+    }
+
 }
 
 function getDefaultDir() {
+    const defaultDir = getSettings().defaultDir
+    if (!defaultDir) {
+        return process.cwd()
+    }
+
+    return isValidDir(defaultDir) ? defaultDir : process.cwd()
 }
 
 function isValidDir(dirPath) {
+    try {
+        fs.readdirSync(dirPath)
+        return true
+    } catch {
+        return false
+    }
 }
 
 module.exports = {
-  getSettings,
-  writeSettings,
-  getDefaultDir,
-  isValidDir
+    getSettings,
+    writeSettings,
+    getDefaultDir,
+    isValidDir
 };
